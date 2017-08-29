54c305a1f99b0927c2e1bdb3bdb2dbae2584356b
JuH33 <julien.boyer49@orange.fr>
2017-08-29 10:28:01 +0200

    Clean Import login

--- app/src/main/java/mapotempo/com/mapotempo_fleet_android/LoginActivity.java
+++ app/src/main/java/mapotempo/com/mapotempo_fleet_android/LoginActivity.java
@@ -1,62 +1,14 @@
 package mapotempo.com.mapotempo_fleet_android;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.mapotempo.fleet.api.MapotempoFleetManagerInterface;
-import com.mapotempo.fleet.core.model.User;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-
-import mapotempo.com.mapotempo_fleet_android.utils.AlertMessageHelper;
-
-import static android.Manifest.permission.READ_CONTACTS;
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 /**
  * A login screen that offers login via email/password.
