d81740dbc3c2f1cb654e5b543038684c94e44377
FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>
2018-06-07 13:57:23 +0200

    Reduce the release timeout.
    Signed-off-by: FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>

--- lib-fleet/src/main/java/com/mapotempo/fleet/core/DatabaseHandler.java
+++ lib-fleet/src/main/java/com/mapotempo/fleet/core/DatabaseHandler.java
@@ -50,11 +50,12 @@ import java.util.concurrent.TimeUnit;
 /**
  * {@inheritDoc}
  */
-public class DatabaseHandler implements IDatabaseHandler {
+public class DatabaseHandler implements IDatabaseHandler
+{
 
     private static String TAG = DatabaseHandler.class.getName();
 
-    final int RELEASE_TIMEOUT = 15;
+    final int RELEASE_TIMEOUT = 2;
 
     private boolean release = false;
 
@@ -76,7 +77,8 @@ public class DatabaseHandler implements IDatabaseHandler {
 
     final private OnCatchLoginError mOnCatchLoginError;
 
-    public interface OnCatchLoginError {
+    public interface OnCatchLoginError
+    {
         void CatchLoginError();
     }
 
@@ -91,7 +93,8 @@ public class DatabaseHandler implements IDatabaseHandler {
     private String PASSWORD_SHA256 = "PASSWORD_SHA256";
 
     public DatabaseHandler(Context context,
-                           String user, String password, String syncGatewayUrl, OnCatchLoginError onCatchLoginError) throws FleetException {
+                           String user, String password, String syncGatewayUrl, OnCatchLoginError onCatchLoginError) throws FleetException
+    {
         mContext = context;
         mUser = user;
         mPassword = password;
@@ -99,51 +102,64 @@ public class DatabaseHandler implements IDatabaseHandler {
         mDatabaseConfiguration = new DatabaseConfiguration(mContext);
         mOnCatchLoginError = onCatchLoginError;
 
-        try {
+        try
+        {
             mDatabase = new Database(mDbname, mDatabaseConfiguration);
-        } catch (CouchbaseLiteException e) {
+        } catch (CouchbaseLiteException e)
+        {
             throw new FleetException("Error : Can't open database");
         }
 
-        try {
+        try
+        {
             mTargetEndpoint = new URLEndpoint(new URI(syncGatewayUrl));
-        } catch (URISyntaxException e) {
+        } catch (URISyntaxException e)
+        {
             throw new FleetException("Error : Invalid url endpoint", e);
         }
 
         mLocalMeta = mDatabase.getDocument("fleet_local_meta");
-        if (mLocalMeta == null) {
+        if (mLocalMeta == null)
+        {
             MutableDocument mutableDocument = new MutableDocument("fleet_local_meta");
-            try {
+            try
+            {
                 mDatabase.save(mutableDocument);
                 mLocalMeta = mutableDocument;
-            } catch (CouchbaseLiteException e) {
+            } catch (CouchbaseLiteException e)
+            {
                 throw new FleetException("Error : Can't create/access to fleet_local_meta_document", e);
             }
         }
     }
 
-    public boolean checkPassword() throws FleetException {
+    public boolean checkPassword() throws FleetException
+    {
         return HashUtils.sha256(mPassword).equals(mLocalMeta.getString(PASSWORD_SHA256));
     }
 
-    public void savePassword() throws FleetException {
+    public void savePassword() throws FleetException
+    {
         MutableDocument mutableDocument = mLocalMeta.toMutable();
         mutableDocument.setString(PASSWORD_SHA256, HashUtils.sha256(mPassword));
-        try {
+        try
+        {
             mDatabase.save(mutableDocument);
             mLocalMeta = mutableDocument;
-        } catch (CouchbaseLiteException e) {
+        } catch (CouchbaseLiteException e)
+        {
             throw new FleetException("Error : Can't save to fleet_local_meta_document", e);
         }
     }
 
-    private String databaseNameGenerator(String userName, String url, String mPassword) throws FleetException {
+    private String databaseNameGenerator(String userName, String url, String mPassword) throws FleetException
+    {
         // Database name must be unique for a username, password and specific url.
         return "database2_" + HashUtils.sha256(userName).substring(0, 5) + HashUtils.sha256(url).substring(0, 5) + HashUtils.sha256(mPassword).substring(0, 5);
     }
 
-    public void configureUserReplication() {
+    public void configureUserReplication()
+    {
         ReplicatorConfiguration replConfig = new ReplicatorConfiguration(mDatabase, mTargetEndpoint);
         replConfig.setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL);
         replConfig.setContinuous(true);
@@ -153,17 +169,21 @@ public class DatabaseHandler implements IDatabaseHandler {
         channels.add("user:" + mUser);
         replConfig.setChannels(channels);
         mUserReplicator = new Replicator(replConfig);
-        ListenerToken tk = mUserReplicator.addChangeListener(new ReplicatorChangeListener() {
+        ListenerToken tk = mUserReplicator.addChangeListener(new ReplicatorChangeListener()
+        {
             @Override
-            public void changed(ReplicatorChange change) {
-                if (change.getStatus().getError() != null) {
+            public void changed(ReplicatorChange change)
+            {
+                if (change.getStatus().getError() != null)
+                {
                     Log.i(TAG, "Error code ::  " + change.getStatus().getError().getCode());
-                    switch (change.getStatus().getError().getCode()) {
-                        case 10401:
-                            mOnCatchLoginError.CatchLoginError();
-                            break;
-                        default:
-                            break;
+                    switch (change.getStatus().getError().getCode())
+                    {
+                    case 10401:
+                        mOnCatchLoginError.CatchLoginError();
+                        break;
+                    default:
+                        break;
                     }
                 }
             }
@@ -173,7 +193,8 @@ public class DatabaseHandler implements IDatabaseHandler {
         mReplicatorList.add(mUserReplicator);
     }
 
-    public void configureCompanyReplication(String companyId) {
+    public void configureCompanyReplication(String companyId)
+    {
         ReplicatorConfiguration replConfig = new ReplicatorConfiguration(mDatabase, mTargetEndpoint);
         replConfig.setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL);
         replConfig.setContinuous(true);
@@ -188,13 +209,15 @@ public class DatabaseHandler implements IDatabaseHandler {
         mReplicatorList.add(mCompanyReplicator);
     }
 
-    public void configureMissionReplication(List<Mission> missions) {
+    public void configureMissionReplication(List<Mission> missions)
+    {
         ReplicatorConfiguration replConfig = new ReplicatorConfiguration(mDatabase, mTargetEndpoint);
         replConfig.setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH_AND_PULL);
         replConfig.setContinuous(true);
         replConfig.setAuthenticator(new BasicAuthenticator(mUser, mPassword));
         List<String> channels = new ArrayList<>();
-        for (Mission d : missions) {
+        for (Mission d : missions)
+        {
             channels.add("mission:" + mUser + ":" + DateUtils.dateForChannel(d.getDate()));
         }
         channels.add("mission:" + mUser + ":" + DateUtils.dateForChannel(-1));
@@ -208,19 +231,27 @@ public class DatabaseHandler implements IDatabaseHandler {
         mReplicatorList.add(mMissionReplicator);
     }
 
-    public void onlineStatus(boolean status) {
-        for (Replicator r : mReplicatorList) {
-            if (!status) {
+    public void onlineStatus(boolean status)
+    {
+        for (Replicator r : mReplicatorList)
+        {
+            if (!status)
+            {
                 r.stop();
-            } else {
+            }
+            else
+            {
                 r.start();
             }
         }
     }
 
-    public boolean isOnline() {
-        for (Replicator r : mReplicatorList) {
-            if (!r.getStatus().getActivityLevel().equals(Replicator.ActivityLevel.STOPPED)) {
+    public boolean isOnline()
+    {
+        for (Replicator r : mReplicatorList)
+        {
+            if (!r.getStatus().getActivityLevel().equals(Replicator.ActivityLevel.STOPPED))
+            {
                 return true;
             }
         }
@@ -234,55 +265,69 @@ public class DatabaseHandler implements IDatabaseHandler {
      * @param deleteDB database deleting option
      * @throws FleetException
      */
-    public void release(final boolean deleteDB) throws FleetException {
+    public void release(final boolean deleteDB) throws FleetException
+    {
         Log.i(TAG, "Log all replicators status: ");
-        for (Replicator r : mReplicatorList) {
+        for (Replicator r : mReplicatorList)
+        {
             printRepl(r);
         }
 
-        for (Replicator r : mReplicatorList) {
-            try {
+        for (Replicator r : mReplicatorList)
+        {
+            try
+            {
                 stopContinuousReplicator(r);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e)
+            {
                 e.printStackTrace();
             }
         }
         Log.i(TAG, "Log all replicators before close database: ");
-        for (Replicator r : mReplicatorList) {
+        for (Replicator r : mReplicatorList)
+        {
             printRepl(r);
         }
-        try {
+        try
+        {
             if (deleteDB)
                 mDatabase.delete();
             else
                 mDatabase.close();
-        } catch (CouchbaseLiteException e) {
+        } catch (CouchbaseLiteException e)
+        {
             // TODO Comment exception propagation
             throw new FleetException("Error during database closing", e);
         }
         release = true;
     }
 
-    private void stopContinuousReplicator(final Replicator repl) throws InterruptedException {
+    private void stopContinuousReplicator(final Replicator repl) throws InterruptedException
+    {
 
         final CountDownLatch latch = new CountDownLatch(1);
-        ListenerToken token = repl.addChangeListener(new ReplicatorChangeListener() {
+        ListenerToken token = repl.addChangeListener(new ReplicatorChangeListener()
+        {
             @Override
-            public void changed(ReplicatorChange change) {
+            public void changed(ReplicatorChange change)
+            {
                 Log.i(TAG, "Log replicator in changelistener: ");
                 printRepl(change.getReplicator());
                 Replicator.Status status = change.getStatus();
-                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED) {
+                if (status.getActivityLevel() == Replicator.ActivityLevel.STOPPED)
+                {
                     latch.countDown();
                 }
             }
         });
         boolean res = false;
-        try {
+        try
+        {
             Log.i(TAG, "Log replicator before stop: ");
             printRepl(repl);
             res = latch.await(RELEASE_TIMEOUT, TimeUnit.SECONDS);
-        } finally {
+        } finally
+        {
             repl.removeChangeListener(token);
         }
         Log.i(TAG, "Log replicator after latch: ");
@@ -290,17 +335,20 @@ public class DatabaseHandler implements IDatabaseHandler {
     }
 
 
-    private void printRepl(Replicator repl) {
+    private void printRepl(Replicator repl)
+    {
         Log.d(TAG, "        replicator ref : " + System.identityHashCode(repl) + " status : " + repl.getStatus().getActivityLevel());
     }
 
     @Override
-    public Database getDatabase() {
+    public Database getDatabase()
+    {
         return mDatabase;
     }
 
     @Override
-    public boolean isRelease() {
+    public boolean isRelease()
+    {
         return release;
     }
 }
